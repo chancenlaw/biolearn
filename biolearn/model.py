186af95abcffcf724a3b1965e8ddfe392a63ab2f
chlaw27 <chancenlaw@college.harvard.edu>
2024-12-17 22:07:15 -1000

    Added clocks

--- biolearn/model.py
+++ biolearn/model.py
@@ -488,6 +488,66 @@ model_definitions = {
             "transform": lambda sum: sum + 55.808884324,
         },
     },
+    "Bohlin": {
+        "year": 2017,
+        "species": "Human",
+        "tissue": "Blood",
+        "source": "https://doi.org/10.2217/epi-2016-0103",
+        "output": "Age (Years)",
+        "model": {
+            "type": "LinearMethylationModel",
+            "file": "Bohlin.csv",
+            "transform": lambda sum: sum + 277.2421,
+        },
+    },
+    "DNAmClockCortical": {
+        "year": 2020,
+        "species": "Human",
+        "tissue": "Blood",
+        "source": "https://doi.org/10.1093/brain/awaa334",
+        "output": "Human Cortex Age (Years)",
+        "model": {
+            "type": "LinearMethylationModel",
+            "file": "DNAmClockCortical.csv",
+            "transform": lambda sum: anti_trafo(sum + 0.577682570446177),
+        },
+    },
+    "Mayne": {
+        "year": 2016,
+        "species": "Human",
+        "tissue": "Placenta",
+        "source": "https://doi.org/10.2217/epi-2016-0103",
+        "output": "Gestational Age",
+        "model": {
+            "type": "LinearMethylationModel",
+            "file": "Mayne.csv",
+            "transform": lambda sum: sum + 24.99026,
+        },
+    },
+    "MiAge": {
+        "year": 2016,
+        "species": "Human",
+        "tissue": "Tumor",
+        "source": "https://doi.org/10.1080/15592294.2017.1389361",
+        "output": "Mitotic Age",
+        "model": {
+            "type": "LinearMethylationModel",
+            "file": "MiAge.csv",
+            "transform": lambda sum: sum,
+        },
+    },
+    "VidalBralo": {
+        "year": 2016,
+        "species": "Human",
+        "tissue": "Blood",
+        "source": "https://doi.org/10.3389/fgene.2016.00126",
+        "output": "Age (Years)",
+        "model": {
+            "type": "LinearMethylationModel",
+            "file": "VidalBralo.csv",
+            "transform": lambda sum: sum + 84.7,
+        },
+    },
 }
 
 
